0e487cef1c6e6baec5bc0219c15effe26252a742
Justinldun <justinldube31@gmail.com>
2023-05-12 14:13:33 +0200

    c1

---
+++ src/test/java/com/enviro/bank/FileProcessing/FileProcessingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enviro.bank.FileProcessing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProcessingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
